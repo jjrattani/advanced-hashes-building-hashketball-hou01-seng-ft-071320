5cc29493fc73f1728113907b39c5a59018820777
Jessica  Rattani <jjrattani@gmail.com>
2020-07-04 20:38:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,23 @@
 # Write your code here!
+def game_hash()
+  return {
+    :home => {
+      :team_name => "", 
+      :colors => [], 
+      :players => [{
+        :player_name,
+        :number,
+        :shoe,
+        :points,
+        :rebounds,
+        :assists,
+        :steals,
+        :blocks,
+        :slam_dunks 
+      }]
+    }
+  }
+  #hash key [:home] => values [:team_name,:colors,:players]
+  #hash key [:away] => values [:team_name,:colors,:players]
+  
+  
